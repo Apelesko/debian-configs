e6484d16718bceaebe3c4d4e6075eda484e9f079
andrew <apelesko@gmail.com>
2023-10-05 19:48:53 -0400

    fixed spelling error, changed pulseaudio for pipewire

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
 #nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
-nala install feh kitty rofi picom thunar lightdm lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libx11-xcb-dev libxft-dev libxinerama-dev libxcb-xinerama0-dev libxcb-res0-dev zram-tools -y
+nala install feh kitty rofi picom thunar lightdm lxpolkit x11-xserver-utils unzip wget pipewire pavucontrol build-essential libx11-dev libx11-xcb-dev libxft-dev libxinerama-dev libxcb-xinerama0-dev libxcb-res0-dev zram-tools -y
 # Installing Other less important Programs
 nala install neofetch lxappearance tldr -y
 
@@ -67,7 +67,7 @@ mv /home/$username/.config/lightdm/lightdm.conf /etc/lightdm
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
-ssystemctl set-default graphical.target
+systemctl set-default graphical.target
 
 # Download, compile, and install DWM
 cd $builddir
@@ -75,7 +75,7 @@ git clone https://github.com/Apelesko/dwm-config
 chown -R $username:$username dwm-config
 mv dwm-config ..
 cd ../dwm-config
-#make install
+make install
 
 # Beautiful bash
 #git clone https://github.com/ChrisTitusTech/mybash
