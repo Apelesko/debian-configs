fea9d0e8d63557598f10980b13509403cc0afac1
andrew <apelesko@gmail.com>
2023-10-10 00:23:56 -0400

    removed linux headers from install script

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
 #nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
-nala install linux-headers-$(uname -r) feh kitty dmenu arandr ufw picom thunar lightdm lxpolkit x11-xserver-utils unzip wget pipewire pavucontrol build-essential libx11-dev libx11-xcb-dev libxft-dev libxinerama-dev libxcb-xinerama0-dev libxcb-res0-dev -y
+nala install feh kitty dmenu arandr ufw picom thunar lightdm lxpolkit x11-xserver-utils unzip wget pipewire pavucontrol build-essential libx11-dev libx11-xcb-dev libxft-dev libxinerama-dev libxcb-xinerama0-dev libxcb-res0-dev -y
 # Installing Other less important Programs
 nala install neofetch lxappearance tldr -y
 
